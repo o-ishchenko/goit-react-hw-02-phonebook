769a3d2e2561831c965f06c7cfdd61dadd55402f
o-ishchenko <olya.galaychuk@gmail.com>
2022-02-15 12:07:40 +0200

    вносит правки

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,30 +1,30 @@
-import React, { Component } from "react";
-import s from './App.module.css'
-import Form from "./Form";
-import Filter from "./Filter";
-import ContactList from "./ContactList";
+import React, { Component } from 'react';
+import s from './App.module.css';
+import Form from './Form';
+import Filter from './Filter';
+import ContactList from './ContactList';
 
 class App extends Component {
-state = {
-  contacts: [{id: 'id-1', name: 'Olga Ishchenko', number: '459-12-56'},
-    {id: 'id-2', name: 'Roman Lysenko', number: '443-89-12'},
-    {id: 'id-3', name: 'Natasha Belodedenko', number: '645-17-79'},
-    { id: 'id-4', name: 'Igor Napolov', number: '227-91-26' },
-    {id: 'id-5', name: 'Vitaliy Gapontsev', number: '827-91-23'},],
-  filter: ''
-}
-  
-  formSubmitHandler = data => { 
-    for (const contact of this.state.contacts) {
-      if (contact.name.toLowerCase() === data.name.toLowerCase()) {
-        alert(`${contact.name} is already in contacts`);
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Olga Ishchenko', number: '459-12-56' },
+      { id: 'id-2', name: 'Roman Lysenko', number: '443-89-12' },
+      { id: 'id-3', name: 'Natasha Belodedenko', number: '645-17-79' },
+      { id: 'id-4', name: 'Igor Napolov', number: '227-91-26' },
+      { id: 'id-5', name: 'Vitaliy Gapontsev', number: '827-91-23' },
+    ],
+    filter: '',
+  };
+
+  formSubmitHandler = data => {
+    if (this.state.contacts.find(contact => contact.name.toLowerCase() === data.name.toLowerCase())) {
+      alert(`${data.name} is already in contacts`);
       return;
-    }
     }
     this.setState(({ contacts }) => ({
       contacts: [data, ...contacts],
     }));
-  }
+  };
   changeFilter = event => {
     this.setState({ filter: event.currentTarget.value });
   };
@@ -33,7 +33,7 @@ state = {
     const { filter, contacts } = this.state;
     const normalizedFilter = filter.toLowerCase();
     return contacts.filter(contact =>
-      contact.name.toLowerCase().includes(normalizedFilter),
+      contact.name.toLowerCase().includes(normalizedFilter)
     );
   };
   deleteContact = contactId => {
@@ -41,8 +41,8 @@ state = {
       contacts: prevState.contacts.filter(contact => contact.id !== contactId),
     }));
   };
-  
-  render() { 
+
+  render() {
     const visibleContacts = this.getVisibleContacts();
 
     return (
@@ -51,11 +51,13 @@ state = {
         <Form onSubmit={this.formSubmitHandler} />
         <h2 className={s.title}>Contacts</h2>
         <Filter value={this.state.filter} onChange={this.changeFilter} />
-        <ContactList contacts={visibleContacts} onDeleteContact={this.deleteContact} />
+        <ContactList
+          contacts={visibleContacts}
+          onDeleteContact={this.deleteContact}
+        />
       </div>
-)
+    );
   }
-
 }
 
- export default App;
+export default App;
--- src/components/ContactList/ContactList.js
+++ src/components/ContactList/ContactList.js
@@ -1,27 +1,25 @@
-import React from "react";
-import PropTypes from "prop-types";
-import s from './ContactList.module.css'
+import React from 'react';
+import PropTypes from 'prop-types';
+import ContactItem from '../ContactItem';
+import s from './ContactList.module.css';
 
-const ContactList = ({contacts, onDeleteContact}) => (
-            <ul className={s.list}>
-        {contacts.map(({ id, name, number}) => (
-            <li className={s.item} key={id}>
-                <h3>{name}</h3>
-                <p>{number}</p>
-                <button onClick={() => onDeleteContact(id)}>Delete</button>
-                  
-            </li>
-          )) }
-        </ul>
-)
+const ContactList = ({ contacts, onDeleteContact }) => (
+  <ul className={s.list}>
+    {contacts.map(({ id, name, number }) => (
+      <li key={id}>
+        <ContactItem name={name} id={id} number={number} onDeleteContact={ onDeleteContact}/>
+      </li>
+    ))}
+  </ul>
+);
 
 ContactList.propTypes = {
-    contacts: PropTypes.arrayOf(
-        PropTypes.shape({
-            id: PropTypes.string.isRequired,
-            name: PropTypes.string.isRequired,
-            number: PropTypes.string.isRequired,
-        })
-    )
-}
-export default ContactList
+  contacts: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      number: PropTypes.string.isRequired,
+    })
+  ),
+};
+export default ContactList;
